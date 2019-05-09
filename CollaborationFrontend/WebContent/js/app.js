9d03aa1a6e7fc43c886657f9f8f16694ebddd0b2
Brightlinpercy <48120576+Brightlinpercy@users.noreply.github.com>
2019-05-09 17:48:56 +0530

    Add files via upload

---
+++ CollaborationFrontend/WebContent/js/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', [ 'ngRoute' ]);
+app.config(function($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl : "home.html"
+	}).when("/login", {
+		templateUrl : "login.html"
+	}).when("/register", {
+		templateUrl : "register.html"
+	}).when("/blog", {
+		templateUrl : "blog.html"
+	}).when("/forum", {
+		templateUrl : "forum.html"
+	}).when("/job", {
+		templateUrl : "job.html"
+	}).when("/showblog", {
+		templateUrl : "viewallblogs.html"
+	}).when("/showforum", {
+		templateUrl : "viewallforum.html"
+	}).when("/showjob", {
+		templateUrl : "viewalljobs.html"
+	})
+});
